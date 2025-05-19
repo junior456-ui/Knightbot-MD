2cfb356490b53bea50d80948e6ff25cd52262d51
junior456-ui <kpossijunior@gmail.com>
2025-05-20 01:52:32 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '~❃𝑺𝑴𝑨𝑹𝑻✦_𝐁𝐀𝐋𝐌𝐀𝐈𝐍_𝐓𝐂❃',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "~❃𝑺𝑴𝑨𝑹𝑻✦_𝐁𝐀𝐋𝐌𝐀𝐈𝐍_𝐓𝐂❃",
+  botOwner: '~❃𝑺𝑴𝑨𝑹𝑻✦_𝐁𝐀𝐋𝐌𝐀𝐈𝐍_𝐓𝐂❃', // Your name
+  ownerNumber: '237687496585', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
